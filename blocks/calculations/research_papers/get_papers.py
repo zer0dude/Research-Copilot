6bfbcce45d0525b3d661a3288d5fd944de70a368
Awos99 <amoswolf@outlook.com>
2024-06-22 19:19:33 +0200

    APA citations

--- blocks/calculations/research_papers/get_papers.py
+++ blocks/calculations/research_papers/get_papers.py
@@ -1,5 +1,12 @@
 from papers_api.scholar_api import get_papers
 import time
+from pybtex.database.input import bibtex
+from pybtex.plugin import find_plugin
+from pybtex.database import BibliographyData
+from pybtex.database.output import bibtex as bibtex_output
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import convert_to_unicode
 
 def get_papers_topics(topics):
     papers = []
@@ -20,3 +27,35 @@ def get_papers_topics(topics):
             seen_ids.add(paper['paperId'])
 
     return unique_papers
+
+
+
+def bibtex_to_apa(bibtex_str, url=None):
+    def format_apa(entry, url=None):
+        authors = entry.get('author', '').replace(' and ', ', ')
+        title = entry.get('title', '')
+        year = entry.get('year', '')
+        journal = entry.get('journal', '')
+        volume = entry.get('volume', '')
+        number = entry.get('number', '')
+        pages = entry.get('pages', '')
+        publisher = entry.get('publisher', '')
+        
+        if entry['ENTRYTYPE'] == 'article':
+            apa_entry = f"{authors} ({year}). {title}. *{journal}*, {volume}({number}), {pages}."
+        elif entry['ENTRYTYPE'] == 'book':
+            apa_entry = f"{authors} ({year}). *{title}*. {publisher}."
+        else:
+            apa_entry = f"{authors} ({year}). {title}."
+        
+        if url:
+            apa_entry += f" [Available online](<{url}>)"
+        
+        return apa_entry
+
+    parser = BibTexParser()
+    parser.customization = convert_to_unicode
+    bib_database = bibtexparser.loads(bibtex_str, parser=parser)
+    
+    apa_entries = [format_apa(entry, url) for entry in bib_database.entries]
+    return '\n'.join(apa_entries)
--- blocks/visualizations/final_papers_list/papers_list.py
+++ blocks/visualizations/final_papers_list/papers_list.py
@@ -1,14 +1,18 @@
 import streamlit as st
-from blocks.calculations.research_papers.get_papers import get_papers_topics
+from blocks.calculations.research_papers.get_papers import get_papers_topics, convert_bibtex, bibtex_to_apa
 
 def get_papers_list():
     if 'relevant_topics' in st.session_state:
         topics_relevant = st.session_state['relevant_topics'].split('\n')[:-1]
-        papers = get_papers_topics(topics_relevant)
-        st.session_state['papers'] = papers
+
+        if 'papers' not in st.session_state:
+            papers = get_papers_topics(topics_relevant)
+            st.session_state['papers'] = papers
+        else:
+            papers = st.session_state['papers']
         
         with st.container(border=True):
             st.subheader('Bibliography')
             for paper in papers:
-                st.markdown(paper['citationStyles']['bibtex'])
-                st.write(paper['title'])
+                st.markdown(bibtex_to_apa(paper['citationStyles']['bibtex'], url=paper['url']))
+                #st.write(paper['title'])
